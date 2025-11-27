ecf6e0f4b8038797d7999151994adcab7803ef19
August <augustgosden@gmail.com>
2025-11-27 14:13:55 +0100

    Mange annotation

--- src/main/java/org/example/projectmanager/controller/TaskController.java
+++ src/main/java/org/example/projectmanager/controller/TaskController.java
@@ -1,4 +1,7 @@
 package org.example.projectmanager.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class TaskController {
 }
--- src/main/java/org/example/projectmanager/repository/ProjectRepository.java
+++ src/main/java/org/example/projectmanager/repository/ProjectRepository.java
@@ -1,4 +1,7 @@
 package org.example.projectmanager.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ProjectRepository {
 }
