6e697c708a5ce8663df9ac3ce68491a370ee0c82
Cell322 <m.stormly@yahoo.dk>
2025-11-28 13:18:57 +0100

    cruddd

--- src/main/java/org/example/projectmanager/repository/ProjectRepository.java
+++ src/main/java/org/example/projectmanager/repository/ProjectRepository.java
@@ -1,7 +1,10 @@
 package org.example.projectmanager.repository;
 
+import org.example.projectmanager.model.Project;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class ProjectRepository {
@@ -10,4 +13,28 @@ public class ProjectRepository {
     public ProjectRepository(JdbcTemplate jdbcTemplate){
         this.jdbcTemplate = jdbcTemplate;
     }
+
+    public Project createProject(Project project){
+        String sqlCreate = "INSERT INTO PROJECT (projectName, projectDescription, estimatedTime, actualTime) VALUES (?, ?, ?, ?)";
+        jdbcTemplate.update(sqlCreate,
+                project.getName(),
+                project.getDescription(),
+                project.getEstimatedTime(),
+                project.getActualTime()
+        );
+        return project;
+    }
+
+    public List<Project> getProjects(){
+        String sql = "SELECT * FROM PROJECT";
+        return jdbcTemplate.query(sql, (rs, rowNum) ->
+        new Project(
+                rs.getInt("projectId"),
+                rs.getString("projectName"),
+                rs.getString("projectDescription"),
+                rs.getInt("estimatedTime"),
+                rs.getInt("actualTime")
+        ));
+    }
+
 }
--- src/main/java/org/example/projectmanager/service/ProjectService.java
+++ src/main/java/org/example/projectmanager/service/ProjectService.java
@@ -1,7 +1,24 @@
 package org.example.projectmanager.service;
 
+import org.example.projectmanager.model.Project;
+import org.example.projectmanager.repository.ProjectRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProjectService {
+
+    ProjectRepository projectRepository;
+    public ProjectService(ProjectRepository projectRepository){
+        this.projectRepository = projectRepository;
+    }
+
+    public Project createProject(Project project){
+        return projectRepository.createProject(project);
+    }
+
+    public List<Project> getProjects(){
+        return projectRepository.getProjects();
+    }
 }
