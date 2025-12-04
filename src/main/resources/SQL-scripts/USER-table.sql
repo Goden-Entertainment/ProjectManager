33244f0265d6447a4040660486d94e5a040e0342
Lornst <augustgosden@gmail.com>
2025-12-04 11:38:49 +0100

    user dev add og edit fix

--- src/main/resources/SQL-scripts/USER-table.sql
+++ src/main/resources/SQL-scripts/USER-table.sql
@@ -3,8 +3,8 @@
 CREATE TABLE IF NOT EXISTS USER (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     userName VARCHAR(255) UNIQUE NOT NULL,
-    userEmail VARCHAR(255) UNIQUE,
-    userPassword VARCHAR(255) NOT NULL,
+    userEmail VARCHAR(255),
+    userPassword VARCHAR(255),
     userType ENUM('ADMIN', 'PROJECTMANAGER', 'DEV') NOT NULL DEFAULT 'DEV',
     devType ENUM('FRONTEND', 'BACKEND', 'FULLSTACK'),
     workTime INT
